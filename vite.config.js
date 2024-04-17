dc02c44cc7568a1db1293bc80fe14047ced35ee0
anzzboy <165716434+anzzboy@users.noreply.github.com>
2024-04-16 18:05:52 -0700

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/store_website_client/",
+  base: "https://anzzboy.github.io/store_website_client/",
 });
